85d4da0a1f0d618c11a4e5b1a96a21b428248b8b
MDaud32 <theprofessor543@gmail.com>
2022-02-21 07:37:49 +0500

    implementing header seciton

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 import './About.scss';
 
+const abouts = [
+  {
+    title: 'Web Development',
+    description: 'i am a good developer',
+    imgURL: '',
+  },
+  {
+    title: 'Web Applications',
+    description: 'i am good at buidling web applications',
+    imgURL: '',
+  },
+  {
+    title: 'UI/Ux Design',
+    description: 'i am a professional UI/UX Designer',
+    imgURL: '',
+  },
+  {
+    title: 'Web Animations',
+    description: 'i will do web animations for your website',
+    imgURL: '',
+  },
+];
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="head-text">
+      I know That
+      <span> Good Design</span>
+      <br />
+      means
+      <span> Good Business</span>
+    </div>
+  );
 }
