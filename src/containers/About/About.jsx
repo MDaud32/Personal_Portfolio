d7a5fdceedd3e0e1a7f90b272cf655392c7f593d
MDaud32 <theprofessor543@gmail.com>
2022-02-21 15:27:06 +0500

    settingup about page & adding content from sanity

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -2,38 +2,45 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import './About.scss';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'i am a good developer',
-    imgURL: '',
-  },
-  {
-    title: 'Web Applications',
-    description: 'i am good at buidling web applications',
-    imgURL: '',
-  },
-  {
-    title: 'UI/Ux Design',
-    description: 'i am a professional UI/UX Designer',
-    imgURL: '',
-  },
-  {
-    title: 'Web Animations',
-    description: 'i will do web animations for your website',
-    imgURL: '',
-  },
-];
+function About() {
+  const [abouts, setAbouts] = useState([]);
 
-export default function About() {
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
-    <div className="head-text">
-      I know That
-      <span> Good Design</span>
-      <br />
-      means
-      <span> Good Business</span>
-    </div>
+    <>
+      <h2 className="head-text">
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 }
+
+export default About;
